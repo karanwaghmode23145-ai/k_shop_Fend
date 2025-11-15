e6f0f9e9ca047e3f75c3735823730ed83e0ad3c6
waghmodekaran5595 <waghmodekaran5595@gmail.com>
2025-11-15 15:12:06 +0530

    updatenew

--- src/components/Header/HeaderMiddle.jsx
+++ src/components/Header/HeaderMiddle.jsx
@@ -3,8 +3,16 @@ import "./HeaderMiddle.css";
 import { FaUser, FaHeart, FaShoppingBag, FaSearch } from "react-icons/fa";
 import logo from "../../assets/logo.png";
 
-
 const HeaderMiddle = () => {
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/login";
+  };
+
   return (
     <div className="header-middle d-none d-lg-block">
       <div className="container position-relative">
@@ -13,45 +21,56 @@ const HeaderMiddle = () => {
 
             {/* Logo */}
             <div className="header-logo">
-              <a href="/">
-                <img src={logo} alt="Site Logo" />
-              </a>
+              <a href="/"><img src={logo} alt="Site Logo" /></a>
             </div>
 
             {/* Search Bar */}
             <div className="header-search">
               <form className="search-form">
-                <input
-                  type="text"
-                  placeholder="Search products..."
-                  className="search-input"
-                />
+                <input type="text" placeholder="Search products..." className="search-input" />
                 <button type="submit" className="search-btn">
                   <FaSearch />
                 </button>
               </form>
             </div>
 
-            {/* User, Wishlist, Cart Icons */}
+            {/* Icons */}
             <div className="header-icons d-flex align-items-center gap-4">
 
+              {/* User / Login / Logout */}
               <div className="header-user">
-                <button className="icon-btn">
-                  <FaUser />
-                </button>
+                {user ? (
+                  <div className="d-flex align-items-center gap-3">
+
+                    <button
+                      className="icon-btn"
+                      onClick={() => window.location.href = "/profile"}
+                    >
+                      <FaUser />
+                      <span className="username">{user.firstName}</span>
+                    </button>
+
+                    <button className="logout-btn" onClick={handleLogout}>
+                      Logout
+                    </button>
+
+                  </div>
+                ) : (
+                  <button className="icon-btn" onClick={() => window.location.href = "/login"}>
+                    <FaUser />
+                  </button>
+                )}
               </div>
 
+              {/* Wishlist */}
               <div className="header-wishlist position-relative">
-                <button className="icon-btn">
-                  <FaHeart />
-                </button>
+                <button className="icon-btn"><FaHeart /></button>
                 <span className="badge">4</span>
               </div>
 
+              {/* Cart */}
               <div className="header-cart position-relative">
-                <button className="icon-btn">
-                  <FaShoppingBag />
-                </button>
+                <button className="icon-btn"><FaShoppingBag /></button>
                 <span className="badge">3</span>
               </div>
 
