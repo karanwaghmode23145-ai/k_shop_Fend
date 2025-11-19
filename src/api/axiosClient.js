5388b0b05a9e20102e4d0536ab1c598c611b89ca
waghmodekaran5595 <waghmodekaran5595@gmail.com>
2025-11-19 20:37:56 +0530

    api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,19 +1,9 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5003",
-  //baseURL: "https://k-shop-bend.vercel.app",
-});
+baseURL: "https://k-shop-bend.vercel.app",
+ //baseURL: "http://localhost:5003",
 
-// 🔥 Automatically attach token with every request
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // ADD TOKEN HERE
-  }
-
-  return config;
 });
 
 export default axiosClient;
